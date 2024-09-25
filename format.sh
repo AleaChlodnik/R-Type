46226867028a0b4181af06786237cf2c0b9fb908
Tom Mendy <tom.mendy@epitech.eu>
2024-09-25 11:42:05 +0200

    [FIX] format.sh
    Update .gitignore to include .vscode directory and modify format.sh to format files in the Client and Interface directories.

--- format.sh
+++ format.sh
@@ -2,8 +2,11 @@
 
 grep_str="^.*.[cpp|hpp]$"
 
-all_client_file="$(find "${PWD}/Server" -maxdepth 5 -type f -not -path '*/\.*' | grep "$grep_str")"
-clang-format -i "$all_client_file"
+all_client_file="$(find "${PWD}/Client" -maxdepth 5 -type f -not -path '*/\.*' | grep "$grep_str")"
+clang-format -i $all_client_file
+
+all_interface_file="$(find "${PWD}/Interface" -maxdepth 2 -type f -not -path '*/\.*' | grep "$grep_str")"
+clang-format -i $all_interface_file
 
 all_server_file="$(find "${PWD}/Server" -maxdepth 5 -type f -not -path '*/\.*' | grep "$grep_str")"
-clang-format -i "$all_server_file"
+clang-format -i $all_server_file
