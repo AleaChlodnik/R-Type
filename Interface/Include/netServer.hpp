8cffe08ac8c07740edb719c1d8da5bb085ae954d
Marc <marc.mendia@epitech.eu>
2024-09-26 19:45:08 +0200

    [MERGE]

--- Interface/Include/netServer.hpp
+++ Interface/Include/netServer.hpp
@@ -21,6 +21,7 @@ template <typename T> class ServerInterface {
               asio::ip::tcp::endpoint(asio::ip::tcp::v4(), port))
     {
     }
+
     virtual ~ServerInterface() { Stop(); }
 
     bool Start()
@@ -29,12 +30,12 @@ template <typename T> class ServerInterface {
             WaitForClientConnection();
 
             m_threadContext = std::thread([this]() { m_asioContext.run(); });
-        } catch (const std::exception &e) {
-            std::cerr << "[SERVER] Exception: " << e.what() << std::endl;
+        } catch (std::exception &e) {
+            std::cerr << "[SERVER] Exception: " << e.what() << "\n";
             return false;
         }
 
-        std::cout << "[SERVER] Started" << std::endl;
+        std::cout << "[SERVER] Started!\n";
         return true;
     }
 
@@ -45,7 +46,7 @@ template <typename T> class ServerInterface {
         if (m_threadContext.joinable())
             m_threadContext.join();
 
-        std::cout << "[SERVER] Stopped" << std::endl;
+        std::cout << "[SERVER] Stopped!\n";
     }
 
     void WaitForClientConnection()
@@ -53,36 +54,43 @@ template <typename T> class ServerInterface {
         m_asioAcceptor.async_accept([this](std::error_code ec,
                                         asio::ip::tcp::socket socket) {
             if (!ec) {
-                std::cout << "[SERVER] New connection:"
-                          << socket.remote_endpoint() << std::endl;
-                std::shared_ptr<connection<T>> newConnection =
-                    std::make_shared<connection<T>>(
-                        connection<T>::owner::server, m_asioContext,
+                std::cout << "[SERVER] New Connection: "
+                          << socket.remote_endpoint() << "\n";
+
+                std::shared_ptr<Connection<T>> newconn =
+                    std::make_shared<Connection<T>>(
+                        Connection<T>::owner::server, m_asioContext,
                         std::move(socket), m_qMessagesIn);
-                if (OnClientConnect(newConnection)) {
-                    m_deqConnections.push_back(std::move(newConnection));
+
+                if (OnClientConnect(newconn)) {
+                    m_deqConnections.push_back(std::move(newconn));
+
                     m_deqConnections.back()->ConnectToClient(nIDCounter++);
+
                     std::cout << "[" << m_deqConnections.back()->GetID()
-                              << "] Connection Approved" << std::endl;
+                              << "] Connection Approved\n";
                 } else {
-                    std::cout << "[------] Connection Denied" << std::endl;
+                    std::cout << "[-----] Connection Denied\n";
                 }
             } else {
-                std::cout << "[SERVER] New connection Error:" << ec.message()
-                          << std::endl;
+                std::cout << "[SERVER] New Connection Error: " << ec.message()
+                          << "\n";
             }
+
             WaitForClientConnection();
         });
     }
 
     void MessageClient(
-        std::shared_ptr<connection<T>> client, const message<T> &msg)
+        std::shared_ptr<Connection<T>> client, const message<T> &msg)
     {
         if (client && client->IsConnected()) {
             client->Send(msg);
         } else {
             OnClientDisconnect(client);
+
             client.reset();
+
             m_deqConnections.erase(std::remove(m_deqConnections.begin(),
                                        m_deqConnections.end(), client),
                 m_deqConnections.end());
@@ -90,7 +98,7 @@ template <typename T> class ServerInterface {
     }
 
     void MessageAllClients(const message<T> &msg,
-        std::shared_ptr<connection<T>> pIgnoreClient = nullptr)
+        std::shared_ptr<Connection<T>> pIgnoreClient = nullptr)
     {
         bool bInvalidClientExists = false;
 
@@ -101,6 +109,7 @@ template <typename T> class ServerInterface {
             } else {
                 OnClientDisconnect(client);
                 client.reset();
+
                 bInvalidClientExists = true;
             }
         }
@@ -111,39 +120,38 @@ template <typename T> class ServerInterface {
                 m_deqConnections.end());
     }
 
-    void Update(size_t nMaxMessage = -1, bool bWait = false)
+    void Update(size_t nMaxMessages = -1, bool bWait = false)
     {
         if (bWait)
             m_qMessagesIn.wait();
+
         size_t nMessageCount = 0;
-        while (nMessageCount < nMaxMessage && !m_qMessagesIn.empty()) {
-            auto msg = m_qMessagesIn.popFront();
+        while (nMessageCount < nMaxMessages && !m_qMessagesIn.empty()) {
+            auto msg = m_qMessagesIn.pop_front();
 
             OnMessage(msg.remote, msg.msg);
+
             nMessageCount++;
         }
     }
 
   protected:
-    virtual bool OnClientConnect(std::shared_ptr<connection<T>> client)
+    virtual bool OnClientConnect(std::shared_ptr<Connection<T>> client)
     {
         return false;
     }
 
-    virtual void OnClientDisconnect(std::shared_ptr<connection<T>> client)
-    {
-        std::cout << "[SERVER] Client disconnected:" << std::endl;
-    }
+    virtual void OnClientDisconnect(std::shared_ptr<Connection<T>> client) {}
 
     virtual void OnMessage(
-        std::shared_ptr<connection<T>> client, message<T> &msg)
+        std::shared_ptr<Connection<T>> client, message<T> &msg)
     {
     }
 
   protected:
     ThreadSafeQueue<owned_message<T>> m_qMessagesIn;
 
-    std::deque<std::shared_ptr<connection<T>>> m_deqConnections;
+    std::deque<std::shared_ptr<Connection<T>>> m_deqConnections;
 
     asio::io_context m_asioContext;
     std::thread m_threadContext;
--- Interface/Include/netThreadSafeQueue.hpp
+++ Interface/Include/netThreadSafeQueue.hpp
@@ -6,6 +6,7 @@
 */
 
 #pragma once
+
 #include "netCommon.hpp"
 
 namespace olc {
@@ -14,7 +15,7 @@ template <typename T> class ThreadSafeQueue {
   public:
     ThreadSafeQueue() = default;
     ThreadSafeQueue(const ThreadSafeQueue<T> &) = delete;
-    virtual ~ThreadSafeQueue() { clear(); };
+    virtual ~ThreadSafeQueue() { clear(); }
 
   public:
     const T &front()
@@ -22,16 +23,38 @@ template <typename T> class ThreadSafeQueue {
         std::scoped_lock lock(muxQueue);
         return deqQueue.front();
     }
+
     const T &back()
     {
         std::scoped_lock lock(muxQueue);
         return deqQueue.back();
     }
+
+    T pop_front()
+    {
+        std::scoped_lock lock(muxQueue);
+        auto t = std::move(deqQueue.front());
+        deqQueue.pop_front();
+        return t;
+    }
+
+    T pop_back()
+    {
+        std::scoped_lock lock(muxQueue);
+        auto t = std::move(deqQueue.back());
+        deqQueue.pop_back();
+        return t;
+    }
+
     void push_back(const T &item)
     {
         std::scoped_lock lock(muxQueue);
         deqQueue.emplace_back(std::move(item));
+
+        std::unique_lock<std::mutex> ul(muxBlocking);
+        cvBlocking.notify_one();
     }
+
     void push_front(const T &item)
     {
         std::scoped_lock lock(muxQueue);
@@ -40,35 +63,25 @@ template <typename T> class ThreadSafeQueue {
         std::unique_lock<std::mutex> ul(muxBlocking);
         cvBlocking.notify_one();
     }
+
     bool empty()
     {
         std::scoped_lock lock(muxQueue);
         return deqQueue.empty();
     }
+
     size_t count()
     {
         std::scoped_lock lock(muxQueue);
         return deqQueue.size();
     }
+
     void clear()
     {
         std::scoped_lock lock(muxQueue);
         deqQueue.clear();
     }
-    T popFront()
-    {
-        std::scoped_lock lock(muxQueue);
-        auto t = std::move(deqQueue.front());
-        deqQueue.pop_front();
-        return t;
-    }
-    T popBack()
-    {
-        std::scoped_lock lock(muxQueue);
-        auto t = std::move(deqQueue.back());
-        deqQueue.pop_back();
-        return t;
-    }
+
     void wait()
     {
         while (empty()) {
@@ -80,7 +93,6 @@ template <typename T> class ThreadSafeQueue {
   protected:
     std::mutex muxQueue;
     std::deque<T> deqQueue;
-
     std::condition_variable cvBlocking;
     std::mutex muxBlocking;
 };
